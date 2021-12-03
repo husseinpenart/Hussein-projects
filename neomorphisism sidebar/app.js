8d4bbdef4beff9cbcaddc9244c29d8d2cfe31447
Hussein <94780245+husseinpenart@users.noreply.github.com>
2021-12-03 15:04:42 +0330

    this is a neomorphisim side bar hope you enjoy
    this is a neomorphisim side bar hope you enjoy ,
    also this is the simple code to build a sidebar in neomorphisim  UI

---
+++ neomorphisism sidebar/app.js
@@ -0,0 +1,13 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width='350px'
+
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.transition = "0.3s linear all";
+  }
+  
+ 
